0c200076ac0b9a773177ea67a1bf2b940ec1f2ab
Fiza srivastava <54671797+Fsrivast@users.noreply.github.com>
2022-02-04 18:22:17 +0530

    Create main.tf

---
+++ Terraform/main.tf
@@ -0,0 +1,20 @@
+#test
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "TerraformDevOps"
+    storage_account_name = "fiservdevopssa"
+    container_name       = "tfstate"
+    key                  = "dev.terraform.tfstate"
+  }
+}
+module "RG" {
+  source   = "./modules/RG" 
+  rgname   = var.rgname     
+  location = var.location
+}
+module "SA" {
+  source   = "./modules/StorageAccount"
+  sname    = var.sname
+  rgname   = var.rgname
+  location = var.location
+}
