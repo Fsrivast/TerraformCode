8a4a638f0d475abd0fdf53b19dd08bb874f59549
Fiza srivastava <54671797+Fsrivast@users.noreply.github.com>
2022-02-07 16:40:41 +0530

    Update variables.tf

--- Terraform/modules/VNET/variables.tf
+++ Terraform/modules/VNET/variables.tf
@@ -13,8 +13,7 @@ variable "address_space" {
   description = "The address space that is used by the virtual network."
   #default     = ["10.0.0.0/16"]
 }
-variable "vnet_location" {
-  description = "The location of the vnet to create. Defaults to the location of the resource group."
+variable "location" {
+  description = "The location of the vnet to create."
   type        = string
-  default     = null
-}
+  }
