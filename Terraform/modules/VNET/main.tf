c8644ddc228cd7c5194f39154793705ba42320d4
Fiza srivastava <54671797+Fsrivast@users.noreply.github.com>
2022-02-07 16:22:06 +0530

    Update main.tf

--- Terraform/modules/VNET/main.tf
+++ Terraform/modules/VNET/main.tf
@@ -2,7 +2,7 @@ resource "azurerm_virtual_network" "vnet" {
   name                = var.vnetname
   location            = var.location
   resource_group_name = var.rgname
-  address_space       = ["10.0.0.0/16"]
+  address_space       = var.address_space
   }
 
 resource "azurerm_subnet" "subnet" {
