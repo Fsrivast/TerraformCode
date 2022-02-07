c6257d281ea5351fb9db3dbedc995c3b9b38035e
Fiza srivastava <54671797+Fsrivast@users.noreply.github.com>
2022-02-07 16:21:33 +0530

    Update variables.tf

--- Terraform/modules/VNET/variables.tf
+++ Terraform/modules/VNET/variables.tf
@@ -1,12 +1,20 @@
-output "vnet_name" {
-  description = "The Name of the newly created vNet"
-  value       = azurerm_virtual_network.vnet.name
+variable "vnet_name" {
+  description = "Name of the vnet to create"
+  type        = string
 }
-output "vnet_location" {
-  description = "The location of the newly created vNet"
-  value       = azurerm_virtual_network.vnet.location
+
+variable "resource_group_name" {
+  description = "Name of the resource group to be imported."
+  type        = string
 }
-output "vnet_id" {
-  description = "The id of the newly created vNet"
-  value       = azurerm_virtual_network.vnet.id
+
+variable "address_space" {
+  type        = list(string)
+  description = "The address space that is used by the virtual network."
+  #default     = ["10.0.0.0/16"]
+}
+variable "vnet_location" {
+  description = "The location of the vnet to create. Defaults to the location of the resource group."
+  type        = string
+  default     = null
 }
