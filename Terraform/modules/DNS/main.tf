0060ae42388476e132666bbbcaf74c6dcd72bfd0
Fiza srivastava <54671797+Fsrivast@users.noreply.github.com>
2022-02-07 21:05:16 +0530

    Update main.tf

--- Terraform/modules/DNS/main.tf
+++ Terraform/modules/DNS/main.tf
@@ -3,3 +3,9 @@ resource "azurerm_private_dns_zone" "dns" {
   resource_group_name = var.rgname
   location            = var.location
   }
+resource "azurerm_private_dns_zone_virtual_network_link" "mylink" {
+  name                  = "mylink"
+  resource_group_name   = var.rgname
+  private_dns_zone_name = azurerm_private_dns_zone.dns.dns_name
+  virtual_network_id    = module.azurerm_virtual_network.vnet_id
+}
