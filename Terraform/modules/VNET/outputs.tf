ede6024e14438f70376e1914f2007a0f85f92a09
Fiza srivastava <54671797+Fsrivast@users.noreply.github.com>
2022-02-07 16:19:13 +0530

    Create outputs.tf

---
+++ Terraform/modules/VNET/outputs.tf
@@ -0,0 +1,12 @@
+output "vnet_name" {
+  description = "The Name of the newly created vNet"
+  value       = azurerm_virtual_network.vnet.name
+}
+output "vnet_location" {
+  description = "The location of the newly created vNet"
+  value       = azurerm_virtual_network.vnet.location
+}
+output "vnet_id" {
+  description = "The id of the newly created vNet"
+  value       = azurerm_virtual_network.vnet.id
+}
