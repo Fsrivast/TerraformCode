9a186e6cf3732270d444d91c5462641ebf226fe1
Fiza srivastava <54671797+Fsrivast@users.noreply.github.com>
2022-02-08 23:24:34 +0530

    Update outputs.tf

--- Terraform/modules/DNS/outputs.tf
+++ Terraform/modules/DNS/outputs.tf
@@ -2,10 +2,6 @@ output "dns_zone" {
   description = "The Name of the newly created dns"
   value       = azurerm_dns_zone.dns.dns_name
 }
-output "location" {
-  description = "The location of the newly created zone"
-  value       = azurerm_virtual_network.dns.location
-}
 output "dns_id" {
   description = "The ID of the newly created dns"
   value       = azurerm_dns_zone.dns.dns_ids
