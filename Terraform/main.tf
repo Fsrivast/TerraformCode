bc96fb213e0daae610b5aae6ad646c352f12bc2f
Fiza srivastava <54671797+Fsrivast@users.noreply.github.com>
2022-02-09 19:52:11 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -17,5 +17,31 @@ module "VNET" {
   rgname   = var.rgname     
   location = var.location
   vname    = var.vnetname
+ }
+module "Subnet" {
+  source   = "./modules/Subnet" 
+  rgname   = var.rgname     
+  location = var.location
   sname    = var.subnetname
-  }
+}
+module "DNS" {
+  source   = "./modules/DNS" 
+  rgname   = var.rgname     
+  location = var.location
+  linkname = var.linkname
+} 
+  module "acr" {
+  source   = "./modules/DNS" 
+  rgname   = var.rgname     
+  location = var.location
+  name = var.acrname
+} 
+module "AKS" {
+  source              = "./modules/AKS"
+  cluster_name        = var.cluster_name
+  kubernetes_version  = var.kubernetes_version
+  location            = var.location
+  rgname              = var.rgname
+  node_resource_group = var.node_resource_group
+  system_node_count   = var.system_node_count
+}
